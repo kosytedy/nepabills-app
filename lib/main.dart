a5fc6f5346dc0545a26196ac67d713f50f600cb3
kosytedy <kosytedy@yahoo.com>
2019-06-24 09:06:15 -0500

    Build Row added

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Welcome to Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: <Widget>[
-              const Icon(Icons.menu),
-              Center(
-                child: Text('NapaBills'),
-              )
-            ],
-          ),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
+      title: "NepaBills",
+      home: RandomWords(),
+      );
   }
 }
 
@@ -39,7 +24,43 @@ class RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              const Icon(Icons.menu),
+              Container(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Text('NapaBills'),
+              )
+            ],
+          ),
+        ),
+        body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
